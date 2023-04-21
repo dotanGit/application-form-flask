f2ca51754d292dd6e6f9519e29a5fa73712d2369
dotan <frenkeldotan1998@gmail.com>
2023-04-21 19:11:12 +0300

    Getting the user data from the input fields

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])   # GET is when the user is accessing the website through the url and POST is when they submit the form
 def index():
+    print(request.method)
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+
     return render_template("index.html")
 
 
